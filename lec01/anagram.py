883df5e931d9d4c77de307d7b1dca513e52ba6f7
田屋　侑希 <yuki@Yuki-2.local>
2019-05-31 12:22:37 +0900

    completed

--- lec01/anagram.py
+++ lec01/anagram.py
@@ -30,6 +30,7 @@ with open('dictionary.sort.json', "r") as f:
 '''
             
 import json
+from time import sleep
 from bs4 import BeautifulSoup
 import urllib.request as req
 import re
@@ -39,62 +40,77 @@ driver = webdriver.Chrome()
 url = "https://icanhazwordz.appspot.com"
 driver.get(url)
 #res = driver.get(url)
-res = req.urlopen(url)
-html = res.read().decode('utf-8')
-soup = BeautifulSoup(html, 'lxml')
+#res = req.urlopen(url)
+#html = res.read().decode('utf-8')
+html_original = driver.page_source
+soup = BeautifulSoup(html_original, 'lxml')
 div = soup.find_all("div")
+for i in range(10):
+    html = driver.page_source
+    if html != html_original:
+        soup = BeautifulSoup(html, 'lxml')
+        div = soup.find_all("div")
+    moji = ''
+    for tag in div:
+        try:
+            string_ = tag.get("class").pop(0)
+            if string_ in "letter":
+                moji += tag.string
+        except:
+            pass
+    moji = moji.lower()
+    print(moji)
 
+    if __name__ == "__main__":
+        answer = ''
+        real_answer = ''
+        counter = 0
+        maximum = 0
+        target = moji
 
-moji = ''
-for tag in div:
-    try:
-        string_ = tag.get("class").pop(0)
-        if string_ in "letter":
-             moji += tag.string
-    except:
-        pass
-moji = moji.lower()
-print(moji)
-
-if __name__ == "__main__":
-    answer = ''
-    real_answer = ''
-    t = []
-    counter = 0
-    maximum = 0
-    #target = input()
-    target = moji
-
-    sorted_target = list(sorted(target))
-    holdtarget = list(sorted(target))
-    with open('dictionary.sortall.json', 'r') as f5:
-        dic = json.load(f5)
-        for i in range(len(dic)):
-            for j in range(len(dic[i][1])):
-                if dic[i][1][j] in sorted_target:
-                    sorted_target.remove(dic[i][1][j])
-                    if j == len(dic[i][1])-1:
-                        answer = dic[i][0]
-                else:
-                    sorted_target = list(sorted(target))
-                    break
-                    
-            if answer != None:
-                for e in answer:
-                    if e in ['j','k','x','z','q']:
-                        counter += 3
-                        if e == 'q':
-                            counter -= 1
-                    elif e in ['c','f','h','l','m','p','v','w','y']:
-                        counter += 2
+        sorted_target = list(sorted(target))
+        holdtarget = list(sorted(target))
+        with open('dictionary.sortall.json', 'r') as f5:
+            dic = json.load(f5)
+            for i in range(len(dic)):
+                for j in range(len(dic[i][1])):
+                    if dic[i][1][j] in sorted_target:
+                        sorted_target.remove(dic[i][1][j])
+                        if j == len(dic[i][1])-1:
+                            answer = dic[i][0]
                     else:
-                        counter += 1
-                if counter >= maximum:
-                    real_answer = answer
-                    maximum = counter
-                counter = 0
-                answer = None
-                sorted_target = holdtarget
-        print(real_answer)
-        driver.find_element_by_name('move').send_keys(real_answer)
-        driver.find_element_by_type('Submit').click()
+                        sorted_target = list(sorted(target))
+                        break
+                if answer != None:
+                    
+                    if 'u' in answer and 'q' in answer:
+                        if sorted_target.count('u') ==  1 and list(answer).count('u') == 1:
+                            answer = ''
+                    if 'u' in answer and 'q' not in answer:
+                        if (answer.count('u')-answer.count('q')) > (target.count('u')-target.count('p')):
+                            answer = ''
+                    if 'q' in answer:
+                        index = answer.index('q')
+                        if answer[index+1] != 'u':
+                            answer = ''
+                
+                if answer != None:
+                    for e in answer:
+                        if e in ['j','k','x','z','q']:
+                            counter += 3
+                            if e == 'q':
+                                counter -= 1
+                        elif e in ['c','f','h','l','m','p','v','w','y']:
+                            counter += 2
+                        else:
+                            counter += 1
+                    if counter >= maximum:
+                        real_answer = answer
+                        maximum = counter
+                    counter = 0
+                    answer = None
+                    sorted_target = holdtarget
+            print(real_answer)
+            driver.find_element_by_name('move').send_keys(real_answer)
+            #driver.find_element_by_value('Submit').click()
+            sleep(2)
